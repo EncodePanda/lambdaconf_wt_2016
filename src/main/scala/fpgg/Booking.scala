be3655d67face3bca59863a0e895ec6473d59f96
Pawel Szulc <paul.szulc@gmail.com>
2017-01-07 13:37:32 -0700

    Add Sandbox to see if all works
    And it does not work.

--- src/main/scala/fpgg/Booking.scala
+++ src/main/scala/fpgg/Booking.scala
@@ -50,3 +50,19 @@ object Functions {
     ((b: Booking) => b.rooms) >>> pickAvailable >>> filterWithView >>> sortByRating >>> (rooms => rooms(0))
 
 }
+
+object Sandbox extends App {
+
+  import Functions._
+
+  val booking = Booking(List(
+    Room("1", 0, view = true, capacity = 5, price = 100, rating = 3.2, booked = false),
+    Room("2", 0, view = true, capacity = 3, price = 150, rating = 9.2, booked = true),
+    Room("3", 0, view = false, capacity = 3, price = 120, rating = 8.4, booked = false),
+    Room("4", 0, view = true, capacity = 4, price = 140, rating = 7.2, booked = false),
+    Room("5", 0, view = true, capacity = 4, price = 140, rating = 4.6, booked = false)
+  ))
+
+  val best: Room = proposeBest(booking)
+  println(best)
+}
