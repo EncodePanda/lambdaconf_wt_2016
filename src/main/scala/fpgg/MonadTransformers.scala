f2b815c1dca27efb86bfbfaf3381fed785d19502
Pawel Szulc <paul.szulc@gmail.com>
2017-01-08 20:01:09 -0700

    Lift value to OptionT

--- src/main/scala/fpgg/MonadTransformers.scala
+++ src/main/scala/fpgg/MonadTransformers.scala
@@ -6,8 +6,9 @@ import Scalaz._
 object MonadTransformers {
 
   type Error[A] = String \/ A
+  type Outcome[A] = OptionT[Error, A]
 
-  def calculate(input: String): OptionT[Error, Int] = ???
+  def calculate(input: String): OptionT[Error, Int] = 10.point[Outcome]
 
   val plus1: OptionT[Error, Int] = for {
     v <- calculate("some")
