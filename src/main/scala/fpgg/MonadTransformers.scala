c9e7b4ae011e176162ba8ef889b9b2b05244d76b
Pawel Szulc <paul.szulc@gmail.com>
2017-01-08 20:02:19 -0700

    Lift inner monad

--- src/main/scala/fpgg/MonadTransformers.scala
+++ src/main/scala/fpgg/MonadTransformers.scala
@@ -8,7 +8,8 @@ object MonadTransformers {
   type Error[A] = String \/ A
   type Outcome[A] = OptionT[Error, A]
 
-  def calculate(input: String): OptionT[Error, Int] = 10.point[Outcome]
+  def calculate(input: String): OptionT[Error, Int] =
+    OptionT[Error, Int](Some(10).right[String])
 
   val plus1: OptionT[Error, Int] = for {
     v <- calculate("some")
